88f1a9a70797c68a00ce62091497299713aa61e3
Ciberaccion <argranados@yahoo.com>
2024-12-03 17:26:52 -0600

    add database entities

---
+++ jobportal-tutorial/src/main/java/com/ciberaccion/jobportal/entity/Users.java
@@ -0,0 +1,26 @@
+package com.ciberaccion.jobportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+@Data
+@NoArgsConstructor
+@ToString
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @ToString.Exclude
+    @OneToMany(targetEntity = Users.class, mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+}
---
+++ jobportal-tutorial/src/main/java/com/ciberaccion/jobportal/entity/UsersType.java
@@ -0,0 +1,37 @@
+package com.ciberaccion.jobportal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@ToString
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date regiatrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+}
