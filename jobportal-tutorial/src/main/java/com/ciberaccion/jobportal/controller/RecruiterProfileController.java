a4564ad840b2a28dee5daceadd3059d07f196361
Ciberaccion <argranados@yahoo.com>
2024-12-16 17:07:57 -0600

    add recruiter profile controller and service

---
+++ jobportal-tutorial/src/main/java/com/ciberaccion/jobportal/controller/RecruiterProfileController.java
@@ -0,0 +1,45 @@
+package com.ciberaccion.jobportal.controller;
+
+
+import com.ciberaccion.jobportal.entity.RecruiterProfile;
+import com.ciberaccion.jobportal.entity.Users;
+import com.ciberaccion.jobportal.repository.UsersRepository;
+import com.ciberaccion.jobportal.services.RecruiterProfileService;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/recruiter-profile")
+public class RecruiterProfileController {
+
+    private final UsersRepository usersRepository;
+    private final RecruiterProfileService recruiterProfileService;
+
+    public RecruiterProfileController(UsersRepository usersRepository,
+    RecruiterProfileService recruiterProfileService) {
+        this.usersRepository = usersRepository;
+        this.recruiterProfileService = recruiterProfileService;
+    }
+
+
+    public String recruiterProfile(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() ->
+                new UsernameNotFoundException("Could not found user"));
+            Optional<RecruiterProfile> recruiterProfile = recruiterProfileService.getOne(users.getUserId());
+
+            recruiterProfile.ifPresent(profile -> model.addAttribute("profile", profile));
+
+        }
+        return "recruiter_profile";
+    }
+}
