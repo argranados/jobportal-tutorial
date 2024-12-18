3a7a5ba1d8a6d53c1f4dbcdb2b2f1f211a9d1f89
Ciberaccion <argranados@yahoo.com>
2024-12-18 17:47:19 -0600

    add Post new job

---
+++ jobportal-tutorial/src/main/java/com/ciberaccion/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.ciberaccion.jobportal.repository;
+
+import com.ciberaccion.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
