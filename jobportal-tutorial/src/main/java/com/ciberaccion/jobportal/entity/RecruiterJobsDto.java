f4b6cb1017a702649a56b8ac4134f503603ea0a8
Ciberaccion <argranados@yahoo.com>
2024-12-19 12:03:29 -0600

    add recruiter dashboard

---
+++ jobportal-tutorial/src/main/java/com/ciberaccion/jobportal/entity/RecruiterJobsDto.java
@@ -0,0 +1,19 @@
+package com.ciberaccion.jobportal.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RecruiterJobsDto {
+
+    private Long totalCandidates;
+    private Integer jobPostId;
+    private String jobTitle;
+    private JobLocation jobLocationId;
+    private JobCompany jobCompanyId;
+}
